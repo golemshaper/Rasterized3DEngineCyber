88934f4f48aae4723f43afe6f2efe6133343fd8a
Bp@ <Brian_Allanson@yahoo.com>
2026-01-16 01:07:50 -0500

    Added a morph example to GameTwo

--- SDL2GameProjectBPA/GameTwo.cpp
+++ SDL2GameProjectBPA/GameTwo.cpp
@@ -3,6 +3,7 @@
 void GameTwo::Tick(float DeltaTime)
 {
 	//Basic example of drawing model to the screen
+	MyScratch->ZWriteOn = false;
 	MyScratch->Clear();
 	totalTime += DeltaTime;
 	MonkeyMesh monkeymesher; //Mesh loading tool
@@ -26,4 +27,15 @@ void GameTwo::Tick(float DeltaTime)
 		 vec3d{ 3,3,3 }
 	 );
 	 MyScratch->AddBuffers();
+
+	 //wave FX
+	 MyScratch->MeshColor = { 0,0,255,255 };
+	 MyScratch->DrawMesh(MyScratch->WaveMesh(monkeymesher.GetMonkeyMesh(), 12 * totalTime, 0.15f), vec3d{ 0.0f,0.0f,-2.25f }, vec3d{ 1.0f, 0.0f, sin(totalTime * 6.0f), }, MyScratch->Lerp(vec3d{ 0.9f,1.2f,0.9f }, vec3d{ 1.2f, 0.9f, 1.2f }, abs(sin(totalTime * 4.0f))), true);
+
+	 //morph FX
+	 MyScratch->MeshColor = { 0,222,255,255 };
+	 Mesh morph = MyScratch->MorphMesh(monkeymesher.GetPyrimidBoxMorph0(), monkeymesher.GetPyrimidBoxMorph(), (sin(totalTime)));
+	 MyScratch->DrawMesh(morph, vec3d{ 1.8f, 0.0f,-2.0f }, vec3d{-1.0f,0,0 }, vec3d{ 0.25f,0.25f,0.25f });
+
+
 }
