596fa97e6a348477a7eb0ceca80090748f1b76fd
Bp@ <Brian_Allanson@yahoo.com>
2026-01-22 22:46:27 -0500

    made example slightly more advanced by adding edge drawing

--- SDL2GameProjectBPA/GameOne.cpp
+++ SDL2GameProjectBPA/GameOne.cpp
@@ -18,9 +18,12 @@ void GameOne::Tick(float DeltaTime)
 	MyScratch->ClearZBufffer();
 	totalTime += DeltaTime;
 	MyScratch->MeshColor = { 222,155,222,255 };
-	MyScratch->DrawVerticies = true;
+	MyScratch->DrawEdges = cos(totalTime*8) < 0;
+	MyScratch->EdgeBrightness = 0.2f;
+	MyScratch->DrawVerticies = sin(totalTime*8) < 0;
+	
 	MyScratch->SetCameraFOV(50);
-	MyScratch->SetCamera(vec3d{ 0.0f, -0.0f, -4.0f }, vec3d{ 0.0f,-0.1f, 1.0f });
+	MyScratch->SetCamera(vec3d{ 0.0f, -0.25f, -4.0f }, vec3d{ 0.0f,-0.1f, 1.0f });
 	MyScratch->DrawMesh(LoadedMesh,vec3d{ 0,0,0 },vec3d{ 1.5, 1.0, totalTime},vec3d{ 3,3,3 },true);
 	MyScratch->DrawMesh(LoadedMesh2, vec3d{ 0.25f,-1.25f,-0.92f }, vec3d{ 1.5, 1.0, totalTime }, vec3d{ 0.15f,0.15f,0.15f }, true);
 
