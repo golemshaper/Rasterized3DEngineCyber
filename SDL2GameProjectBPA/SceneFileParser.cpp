2b6bbf4f8e8b2b1a8d6acbd651f7f741de6b73e1
Bp@ <Brian_Allanson@yahoo.com>
2026-02-02 21:18:10 -0500

    Sketching out parser

--- SDL2GameProjectBPA/SceneFileParser.cpp
+++ SDL2GameProjectBPA/SceneFileParser.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <fstream>
 
-void SceneFileParser::ParseSceneFromFile(const std::string path)
+void SceneFileParser::ParseSceneFromFile(const std::string path, const std::string mesh_asset_path_root)
 {
 	std::ifstream file(path);
 	std::string str;
@@ -17,9 +17,82 @@ void SceneFileParser::ParseSceneFromFile(const std::string path)
 		file_contents += str;
 		file_contents.push_back('\n');
 	}
-	ParseSceneFromString(file_contents);
+	ParseSceneFromString(file_contents, mesh_asset_path_root);
 }
 
-void SceneFileParser::ParseSceneFromString(const std::string str)
+void SceneFileParser::ParseSceneFromString(const std::string str, const std::string mesh_asset_path_root)
 {
+	
+	/*NEW DRAFT:
+	----------------------------------------------------------------------------------------
+					Positions:
+					0.000 0.000 0.000
+					2.000 0.000 1.500
+					Rotations:
+					0.000 0.000 0.000
+					Scales:
+					1,1,1
+
+					Models:
+					#Model, Texture, Location, Rotation, Scale ID, visibility, Misc Tags
+					Human,BasicPaletteTexture,1,0,0,true,Tag1,Tag2
+
+					DialogueTriggers:
+					"WelcomeToTheWorld",model ID
+	----------------------------------------------------------------------------------------
+	*/
+
+	std::string sb;
+	sb.reserve(1024);
+
+	//Mode states:
+	const int M_Unknown = 0;
+	const int M_Positions = 1;
+	const int M_Rotations = 2;
+	const int M_Scales = 3;
+	int Mode = M_Unknown;
+	bool Comment_Mode = false;
+
+	for (std::size_t i = 0; i < str.size(); ++i)
+	{
+		char c = str[i];
+
+		if (c == '#')
+		{
+			Comment_Mode = true;
+		}
+		if (c == ':' && Comment_Mode == false)
+		{
+			//a new section was marked in the file via a ':' .
+			//such as "positions:" so find out what the name was by reading sb
+			if ((sb) == "Positions")
+			{
+				Mode = M_Positions;
+			}
+			if ((sb) == "Rotations")
+			{
+				Mode = M_Rotations;
+			}
+			if ((sb) == "Scales")
+			{
+				Mode = M_Scales;
+			}
+		}
+		if (c == '\n')
+		{
+			Comment_Mode = false;
+			switch (Mode)
+			{
+				case M_Positions:
+				//Build positions
+				break;
+				case M_Rotations:
+				//Build rotations
+				break;
+				case M_Scales:
+				//Build scales
+				break;
+			}
+		}
+	}
 }
--- SDL2GameProjectBPA/SceneFileParser.h
+++ SDL2GameProjectBPA/SceneFileParser.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <string>
-
+#include "DrawScratchSpace.h"
+struct SceneObject {
+	std::string AssetPath; //The name of the mesh asset. 
+	Mesh model; //should this need to be explicitly loaded by the programmer?
+	vec3d pos;
+	vec3d rot;
+	vec3d scale;
+	bool visible;
+	std::vector<std::string> tags;
+};
 class SceneFileParser
 {
-	void ParseSceneFromFile(const std::string path);
-	void ParseSceneFromString(const std::string str);
+	void ParseSceneFromFile(const std::string path, const std::string mesh_asset_path_root);
+	void ParseSceneFromString(const std::string str, const std::string mesh_asset_path_root);
 };
 
