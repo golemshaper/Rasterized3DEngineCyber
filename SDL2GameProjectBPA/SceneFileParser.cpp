0418a5618b87aa2af508059b52c5e7738d7cd66a
Bp@ <Brian_Allanson@yahoo.com>
2026-02-03 01:29:52 -0500

    Scene parser work continues

--- SDL2GameProjectBPA/SceneFileParser.cpp
+++ SDL2GameProjectBPA/SceneFileParser.cpp
@@ -1,6 +1,8 @@
 #include "SceneFileParser.h"
 #include <string>
+#include <sstream>
 #include <fstream>
+#include <map>
 
 void SceneFileParser::ParseSceneFromFile(const std::string path, const std::string mesh_asset_path_root)
 {
@@ -33,12 +35,12 @@ void SceneFileParser::ParseSceneFromString(const std::string str, const std::str
 					Scales:
 					1,1,1
 
-					Models:
+					Objects:
 					#Model, Texture, Location, Rotation, Scale ID, visibility, Misc Tags
 					Human,BasicPaletteTexture,1,0,0,true,Tag1,Tag2
 
 					DialogueTriggers:
-					"WelcomeToTheWorld",model ID
+					"WelcomeToTheWorld",obj ID
 	----------------------------------------------------------------------------------------
 	*/
 
@@ -50,8 +52,31 @@ void SceneFileParser::ParseSceneFromString(const std::string str, const std::str
 	const int M_Positions = 1;
 	const int M_Rotations = 2;
 	const int M_Scales = 3;
+	const int M_Objects = 4;
+	const int M_Dialogue= 5;
+
 	int Mode = M_Unknown;
 	bool Comment_Mode = false;
+
+	//It seems silly to keep these vectors as different catagories, but it's proably easier to hand edit a scene, should the need arise...
+	std::vector<vec3d> postion_vectors;
+	std::map<int, vec3d > id_positions_map;
+
+	std::vector<vec3d> rotation_vectors;
+	std::map<int, vec3d > id_rotations_map;
+
+	std::vector<vec3d> scale_vectors;
+	std::map<int, vec3d > id_scales_map;
+
+
+	std::vector<SceneObject> scene_objects;
+
+	std::vector<DialogueTriggers> dialogue_triggers;
+
+	vec3d working_vec{ 0,0,0 };
+	vec3d working_vec2{ 0,0,0 };
+	vec3d working_vec3{0,0,0};
+	SceneObject working_scene_object;
 
 	for (std::size_t i = 0; i < str.size(); ++i)
 	{
@@ -77,22 +102,132 @@ void SceneFileParser::ParseSceneFromString(const std::string str, const std::str
 			{
 				Mode = M_Scales;
 			}
+			if ((sb) == "Objects")
+			{
+				Mode = M_Objects;
+			}
+			if ((sb) == "DialogueTriggers")
+			{
+				Mode = M_Dialogue;
+			}
 		}
 		if (c == '\n')
 		{
-			Comment_Mode = false;
+			sb = trim(sb); //Trim this string on both sides.
+			std::vector<std::string>  results = SplitByChar(sb, ' ');
+			int lineIndex_pos = postion_vectors.size();
+			int lineIndex_rot = rotation_vectors.size();
+			int lineIndex_scl = scale_vectors.size();
+
+
+			int posIndex = 0;
+			int rotIndex = 0;
+			int sclIndex = 0;
+			int model_id_holder = 0;
+			int texture_id_holder = 0;
+			bool working_visible = true;
+			std::string assetName;
+			std::string textureName;
+			std::vector<std::string> tags;
+
+
+			Comment_Mode = false; //comments have ended
 			switch (Mode)
 			{
 				case M_Positions:
-				//Build positions
+					//Build positions
+					working_vec = vec3d{ std::stof((results[0])), std::stof((results[1])), std::stof((results[2])) };
+					postion_vectors.push_back(working_vec);
+					id_positions_map[lineIndex_pos] = working_vec;
 				break;
 				case M_Rotations:
-				//Build rotations
+					//Build rotations
+					working_vec = vec3d{ std::stof((results[0])), std::stof((results[1])), std::stof((results[2])) };
+					rotation_vectors.push_back(working_vec);
+					id_rotations_map[lineIndex_rot] = working_vec;
+
 				break;
 				case M_Scales:
-				//Build scales
+					//Build scales
+					working_vec = vec3d{ std::stof((results[0])), std::stof((results[1])), std::stof((results[2])) };
+					scale_vectors.push_back(working_vec);
+					id_scales_map[lineIndex_scl] = working_vec;
+
 				break;
+				case M_Objects:
+					//Build objects
+					//ASSUME INPUT LIKE:
+					//"Human, BasicPaletteTexture, 1, 0, 0, true, Tag1, Tag2"
+					
+					assetName = results[0];
+					textureName = results[1];
+
+					posIndex = std::stoi(results[2]);
+					rotIndex = std::stoi(results[3]);
+					sclIndex = std::stoi(results[4]);
+					bool visible = (results[5] == "true"); //5 is the index for the visible bool! If it changes, make a constant for it.
+					for (int i = 6; i < results.size(); ++i) //6 is the start of the tags. if we add or remove any properties, change this and make a const for it.
+					{
+						tags.push_back(results[i]);
+					}
+
+					working_scene_object = {
+						assetName,
+						GetModelID(assetName),
+						GetTextureId(textureName),
+						id_positions_map[posIndex],
+						id_rotations_map[rotIndex],
+						id_scales_map[sclIndex],
+						working_visible,
+						tags
+					};
+					
+					scene_objects.push_back(working_scene_object);
+
+
+				break;
+				//case M_Dialogue:
+				//	//Build dialogue
+
+				//break;
 			}
 		}
+
+		if (!Comment_Mode)
+		{
+			sb += c; //continue scanning 
+		}
+
 	}
+}
+int SceneFileParser::GetModelID(const std::string str)
+{
+	return 0;
+}
+int SceneFileParser::GetTextureId(const std::string str)
+{
+	return 0;
+}
+std::vector<std::string> SceneFileParser::SplitByChar(const std::string& str, char c)
+{
+	std::vector<std::string> strings;
+	std::istringstream f(str);
+	std::string s;
+	while (std::getline(f, s, c)) {
+		strings.push_back(s);
+	}
+	return strings;
+}
+
+std::string SceneFileParser::trim(const std::string& s)
+{
+	size_t start = 0;
+	while (start < s.size() && std::isspace(static_cast<unsigned char>(s[start])))
+		++start;
+
+	size_t end = s.size();
+	while (end > start && std::isspace(static_cast<unsigned char>(s[end - 1])))
+		--end;
+
+	return s.substr(start, end - start);
 }
--- SDL2GameProjectBPA/SceneFileParser.h
+++ SDL2GameProjectBPA/SceneFileParser.h
@@ -1,18 +1,40 @@
 #pragma once
 #include <string>
 #include "DrawScratchSpace.h"
+#include <map>
 struct SceneObject {
 	std::string AssetPath; //The name of the mesh asset. 
-	Mesh model; //should this need to be explicitly loaded by the programmer?
+	int model_id; //should this need to be explicitly loaded by the programmer?
+	int texture_id; //id of the texture map
 	vec3d pos;
 	vec3d rot;
 	vec3d scale;
 	bool visible;
 	std::vector<std::string> tags;
 };
+struct TexturePack {
+	std::string Texture;
+	int width;
+	int height;
+};
+struct Scene {
+	std::vector<SceneObject> scene_objects;
+	std::map<int, TexturePack > texture_pack_map;
+};
+struct DialogueTriggers {
+	std::string dialogue;
+	int model_id;
+};
+
 class SceneFileParser
 {
 	void ParseSceneFromFile(const std::string path, const std::string mesh_asset_path_root);
 	void ParseSceneFromString(const std::string str, const std::string mesh_asset_path_root);
+	int GetModelID(const std::string str);
+	int GetTextureId(const std::string str);
+	std::vector<std::string> SplitByChar(const std::string& str, char c);
+	std::string trim(const std::string& s);
+
+
 };
 
